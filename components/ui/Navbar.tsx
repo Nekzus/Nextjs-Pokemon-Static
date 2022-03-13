9deeba3375220208f339a6c915cf2b9ab6be14a8
Nekzus <85507589+Nekzus@users.noreply.github.com>
2022-03-13 04:32:33 -0300

    Primer commit

---
+++ components/ui/Navbar.tsx
@@ -0,0 +1,18 @@
+
+
+export const Navbar = () => {
+    return (
+        <div style={{
+            display: 'flex',
+            width: '100%',
+            flexDirection: 'row',
+            alignItems: 'center',
+            justifyContent: 'start',
+            padding: '0px 20px',
+            backgroundColor: 'red',
+
+        }}>
+            <span>Hola</span>
+        </div>
+    )
+};
